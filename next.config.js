78a592b2a82dadf161d35cc0a3f88a47ba1a6860
Paulo <paulosoares.other@gmail.com>
2023-12-05 19:58:13 -0300

    My version

--- next.config.js
+++ next.config.js
@@ -2,14 +2,22 @@
 const nextConfig = {
     images: {
         remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'gateway.pinata.cloud',
-            port: '',
-            pathname: '/ipfs/**',
-          },
+            {
+                protocol: "https",
+                hostname: "gateway.pinata.cloud",
+                port: "",
+                pathname: "/ipfs/**",
+            },
         ],
-      },
-}
+    },
+    env: {
+        APP_DOMAIN: process.env.APP_DOMAIN,
+        MORALIS_API_KEY: process.env.MORALIS_API_KEY,
+        NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+        NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+        NEXT_PUBLIC_PINATA_API_KEY: process.env.NEXT_PUBLIC_PINATA_API_KEY,
+        NEXT_PUBLIC_ALCHEMY_API_URL: process.env.NEXT_PUBLIC_ALCHEMY_API_URL,
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
